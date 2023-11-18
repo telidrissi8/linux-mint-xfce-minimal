512016d96709b6d267bdb9aa11234f52908d448d
trk-tech <130991878+trk-tech@users.noreply.github.com>
2023-11-18 08:47:26 -0500

    final

--- mintDebloat.sh
+++ mintDebloat.sh
@@ -14,21 +14,18 @@ echo -e "yes\nyes\nno" | sudo apt-get purge metacity* || true
 echo -e "yes\nyes\nno" | sudo apt-get purge compton* || true
 
 # Uninstall firefox
-echo -e "yes\nyes\nno" | sudo apt-get purge firefox*
+echo -e "yes\nyes\nno" | sudo apt-get purge firefox* || true
 
 # Other
 echo -e "yes\nyes\nno" | sudo apt-get purge rhythmbox* || true
-echo -e "yes\nyes\nno" | sudo apt-get purge celluloid* || true
 echo -e "yes\nyes\nno" | sudo apt-get purge hexchat* || true
 echo -e "yes\nyes\nno" | sudo apt-get purge thunderbird* || true
 echo -e "yes\nyes\nno" | sudo apt-get purge transmission* || true
 echo -e "yes\nyes\nno" | sudo apt-get purge simple-scan* || true
 echo -e "yes\nyes\nno" | sudo apt-get purge sticky* || true
-echo -e "yes\nyes\nno" | sudo apt-get purge "*flatpak*" || true
 echo -e "yes\nyes\nno" | sudo apt autoremove --purge snapd || true
 echo -e "yes\nyes\nno" | sudo apt-mark hold snapd || true
 echo -e "yes\nyes\nno" | sudo apt autoremove --purge xfburn || true
-echo -e "yes\nyes\nno" | sudo apt autoremove --purge remmina || true
 echo -e "yes\nyes\nno" | sudo apt autoremove --purge gimp || true
 echo -e "yes\nyes\nno" | sudo apt autoremove --purge parole || true
 
@@ -39,35 +36,47 @@ echo -e "yes\nyes\nno" | sudo apt autoremove --purge hypnotix || true
 sudo apt-get clean || true
 sudo apt-get autoremove || true
 
-echo -e "yes\nyes\nno" | sudo apt-get install vlc || true
 echo -e "yes\nyes\nno" | sudo apt-get install deborphan || true
 echo -e "yes\nyes\nno" | sudo apt-get install bleachbit || true
-echo -e "yes\nyes\nno" | sudo apt-get install tlp || true
-echo -e "yes\nyes\nno" | sudo apt-get install powertop || true
-echo -e "yes\nyes\nno" | sudo apt install sublime-text || true
 
 # Chrome
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 echo -e "\n" | apt install ./google-chrome-stable_current_amd64.deb
 rm -f ./google-chrome-stable_current_amd64.deb
 
-# nodeJs
-curl -fsSL https://deb.nodesource.com/setup_18.x | sudo -E bash -
-echo -e "yes\nyes\nno" | sudo apt-get install -y nodejs || true
+while [ -n "`deborphan`" ]; do
+    deborphan
+    echo -e "yes\nyes\nno" | sudo apt purge `deborphan`
+done
 
-# vscode
-sudo apt-get install wget gpg
-wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
-sudo install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg
-sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
-rm -f packages.microsoft.gpg
-sudo apt install apt-transport-https
-sudo apt update
-sudo apt install code
 
-#git
-echo -e "yes\nyes\nno" | sudo apt install git || true
-ssh-keygen
-cat ~/.ssh/id_rsa.pub
-read -p "Please add the pub key to gitlab." yn
+# Loop through all provided parameters
+while [ "$#" -gt 0 ]; do
+    param="$1"
 
+    # Execute a specific portion of code based on the parameter
+    case "$param" in
+        laptop)
+            echo "Executing code for laptop mode..."
+            echo -e "yes\nyes\nno" | sudo apt-get install tlp || true
+			echo -e "yes\nyes\nno" | sudo apt-get install powertop || true
+            ;;
+
+        dev)
+            echo "Executing code for dev mode..."
+            flatpak install flathub -y org.gnome.meld
+			flatpak install flathub -y com.sublimetext.three
+			flatpak install flathub -y com.visualstudio.code
+            ;;
+
+        *)
+            echo "Invalid parameter: $param. Please use 'laptop' or 'dev'."
+            exit 1
+            ;;
+    esac
+
+    # Shift to the next parameter
+    shift
+done
+
+flatpak install flathub -y io.gitlab.librewolf-community
